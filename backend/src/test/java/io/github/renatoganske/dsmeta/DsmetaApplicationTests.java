41fb099bc7c9775c694517c752eedff0f4cf82cb
Renato Ganske Junior <renatoganske.jr@gmail.com>
2022-11-14 23:29:27 -0300

    Projeto criado

---
+++ backend/src/test/java/io/github/renatoganske/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.renatoganske.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
